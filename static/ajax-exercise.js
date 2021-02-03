cb7271018fcadc73ced983ff5d0cdb31f7854288
Jen <brissman514@gmail.com>
2021-02-03 02:12:52 +0000

    initial commit

---
+++ static/ajax-exercise.js
@@ -0,0 +1,50 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+function replaceFortune(res) {
+    $("#fortune-text").html(res);
+}
+
+function showFortune(evt) {
+    $.get('/fortune', replaceFortune);
+
+}
+
+$('#get-fortune-button').on('click', showFortune);
+
+
+// PART 2: SHOW WEATHER
+
+function replaceForecast(weather) {
+    $("#weather-info").html(weather.forecast);
+}
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    let url = "/weather.json";
+    let formData = {"zipcode": $("#zipcode-field").val()};
+
+    $.get(url, formData, replaceForecast);
+
+    // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+$("#weather-form").on('submit', showWeather);
+
+
+
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
